ab63298eacdb51646115108ce2d588b1b106bad6
Andrew Jones <ajones@digitalbazaar.com>
2022-09-23 14:03:51 -0400

    Add eslint for tests & further work on create test suite.

--- tests/10-create.js
+++ tests/10-create.js
@@ -3,4 +3,27 @@
  */
 
 import chai from 'chai';
-import {checkDataIntegrityProofFormat} from 'data-integrity-test-suite-assertion';
+import {
+  checkDataIntegrityProofFormat
+} from 'data-integrity-test-suite-assertion';
+import {endpoints} from 'vc-api-test-suite-implementations';
+import {generateTestData} from './vc-generator/index.js';
+import {klona} from 'klona';
+
+const tag = 'eddsa-2022';
+const {match, nonMatch} = endpoints.filterByTag({tags: [tag], property: 'issuers'});
+const should = chai.should();
+const bs58 = /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+$/;
+
+describe('eddsa-2022 (create)', function() {
+  let validVc;
+  before(async function() {
+    const credentials = await generateTestData();
+    validVc = credentials.get('validVc');
+  });
+  checkDataIntegrityProofFormat({
+    implemented: match,
+    notImplemented: nonMatch,
+    tag
+  });
+});
