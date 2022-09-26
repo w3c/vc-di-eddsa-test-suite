c0db0abb88bceca923d903a3bd47f4cf820f9fbd
Andrew Jones <ajones@digitalbazaar.com>
2022-09-26 12:59:29 -0400

    Await call to issuer.

--- tests/helpers.js
+++ tests/helpers.js
@@ -5,13 +5,13 @@
 import {klona} from 'klona';
 import {v4 as uuidv4} from 'uuid';
 
-export const createInitialVc = ({issuer, vc}) => {
+export const createInitialVc = async ({issuer, vc}) => {
   const {settings: {id: issuerId, options}} = issuer;
   const credential = klona(vc);
   credential.id = `urn:uuid:${uuidv4()}`;
   credential.issuer = issuerId;
   const body = {credential, options};
-  const {data, error} = issuer.post({json: body});
+  const {data, error} = await issuer.post({json: body});
   if(error) {
     throw error;
   }
