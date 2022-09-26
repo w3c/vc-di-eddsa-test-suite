1659f1ee32a3441757d118fd8016ccb9c1aba8b4
Andrew Jones <ajones@digitalbazaar.com>
2022-09-26 14:58:06 -0400

    Move helpers over and test more.

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,9 +1,17 @@
 /*!
  * Copyright 2022 Digital Bazaar, Inc. All Rights Reserved
  */
-
+import * as didKey from '@digitalbazaar/did-method-key';
+import {IdDecoder, IdEncoder} from 'bnid';
 import {klona} from 'klona';
 import {v4 as uuidv4} from 'uuid';
+import varint from 'varint';
+
+const didKeyDriver = didKey.driver();
+const decoder = new IdDecoder({
+  encoding: 'base58',
+  multibase: true
+});
 
 export const createInitialVc = async ({issuer, vc}) => {
   const {settings: {id: issuerId, options}} = issuer;
@@ -17,3 +25,24 @@ export const createInitialVc = async ({issuer, vc}) => {
   }
   return data;
 };
+
+// base58, multibase, fixed-length encoder
+const encoder = new IdEncoder({
+  encoding: 'base58',
+  multibase: true
+});
+
+export const getPublicKeyBytes = async ({did}) => {
+  const didDoc = await didKeyDriver.get({did});
+  const multiCodecBytes = decoder.decode(didDoc.publicKeyMultibase);
+  // extracts the varint bytes
+  varint.decode(multiCodecBytes);
+  // how many bytes were used to specify the size of the key material
+  const varBytes = varint.decode.bytes;
+  // return just the key material
+  return multiCodecBytes.slice(varBytes, multiCodecBytes.length);
+};
+
+export const bs58Decode = ({id}) => decoder.decode(id);
+
+export const bs58Encode = data => encoder.encode(data);
