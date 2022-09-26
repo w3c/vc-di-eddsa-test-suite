b25693e2df80e2f50cdc33c51e713ec6d4ca918d
Andrew Jones <ajones@digitalbazaar.com>
2022-09-26 14:58:06 -0400

    Move helpers over and test more.

--- tests/10-create.js
+++ tests/10-create.js
@@ -2,16 +2,16 @@
  * Copyright 2022 Digital Bazaar, Inc. All Rights Reserved
  */
 
+import {bs58Decode, createInitialVc, getPublicKeyBytes} from './helpers.js';
 import chai from 'chai';
 import {
   checkDataIntegrityProofFormat
 } from 'data-integrity-test-suite-assertion';
-import {createInitialVc} from './helpers.js';
 import {endpoints} from 'vc-api-test-suite-implementations';
 import {generateTestData} from './vc-generator/index.js';
-import {klona} from 'klona';
 
 const tag = 'eddsa-2022';
+const cryptosuite = 'eddsa-2022';
 const {match, nonMatch} = endpoints.filterByTag({
   tags: [tag],
   property: 'issuers'
@@ -57,6 +57,72 @@ describe('eddsa-2022 (create)', function() {
           'Expected at least one proof to have cryptosuite.'
         );
       });
+      it('The field "cryptosuite" MUST be `eddsa-2022`', function() {
+        this.test.cell = {columnId, rowId: this.test.title};
+        proofs.some(
+          proof => proof?.cryptosuite === cryptosuite
+        ).should.equal(
+          true,
+          'Expected at least one proof to have "cryptosuite" `eddsa-2022`.'
+        );
+      });
+      it('"proofValue" field MUST exist and be a multibase-encoded ' +
+          'base58-btc encoded value', function() {
+        this.test.cell = {columnId, rowId: this.test.title};
+        const multibase = 'z';
+        proofs.some(proof => {
+          const value = proof?.proofValue || '';
+          return value.startsWith(multibase) && bs58.test(value);
+        }).should.equal(
+          true,
+          'Expected "proof.proofValue" to be multibase-encoded base58-btc ' +
+          'value.'
+        );
+      });
+      it('"proofValue" field when decoded to raw bytes, MUST be 64 bytes ' +
+        'in length if the associated public key is 32 bytes or 114 bytes ' +
+        'in length if the public key is 57 bytes.', async function() {
+        this.test.cell = {columnId, rowId: this.test.title};
+        should.exist(issuedVc, 'Expected issuer to have issued a ' +
+          'credential.');
+        should.exist(proofs, 'Expected credential to have a proof.');
+        const eddsa2022Proofs = proofs.filter(
+          proof => proof?.cryptosuite === cryptosuite);
+        eddsa2022Proofs.length.should.be.gte(1, 'Expected at least one ' +
+          'Ed25519 proof.');
+        for(const proof of eddsa2022Proofs) {
+          should.exist(proof.proofValue, 'Expected a proof value on ' +
+            'the proof.');
+          const valueBytes = bs58Decode({id: proof.proofValue});
+          should.exist(proof.verificationMethod);
+          const vmBytes = await getPublicKeyBytes({
+            did: proof.verificationMethod});
+          vmBytes.byteLength.should.be.oneOf([32, 57], 'Expected public ' +
+            'key bytes to be either 32 or 57 bytes.');
+          if(vmBytes.byteLength === 32) {
+            valueBytes.byteLength.should.equal(64, 'Expected 64 bytes ' +
+              'proofValue for 32 bytes key.');
+          } else {
+            valueBytes.byteLength.should.equal(114, 'Expected 114 bytes ' +
+              'proofValue for 57 bytes key.');
+          }
+        }
+      });
+      it('"proof" MUST verify when using a conformant verifier.',
+        async function() {
+          this.test.cell = {columnId, rowId: this.test.title};
+          should.exist(verifier, 'Expected implementation to have a VC ' +
+            'HTTP API compatible verifier.');
+          const {result, error} = await verifier.post({json: {
+            verifiableCredential: issuedVc,
+            options: {checks: ['proof']}
+          }});
+          should.not.exist(error, 'Expected verifier to not error.');
+          should.exist(result, 'Expected verifier to return a result.');
+          result.status.should.not.equal(400, 'Expected status code to not ' +
+            'be 400.');
+          result.status.should.equal(200, 'Expected status code to be 200.');
+        });
     }
   });
 });
