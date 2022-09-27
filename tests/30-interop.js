209d80fd16274a88dff46a1a82c94cfd450ea9a0
Andrew Jones <ajones@digitalbazaar.com>
2022-09-27 10:17:55 -0400

    Add interop tests for eddsa-2022.

---
+++ tests/30-interop.js
@@ -0,0 +1,58 @@
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+import chai from 'chai';
+import {createInitialVc} from './helpers.js';
+import {endpoints} from 'vc-api-test-suite-implementations';
+import {generateTestData} from './vc-generator/index.js';
+
+const should = chai.should();
+const tag = 'eddsa-2022';
+
+// only use implementations with `eddsa-2022` issuers.
+const {
+  match: issuerMatches
+} = endpoints.filterByTag({tags: [tag], property: 'issuers'});
+const {
+  match: verifierMatches
+} = endpoints.filterByTag({tags: [tag], property: 'verifiers'});
+
+describe('eddsa-2022 (interop)', function() {
+  let validVc;
+  before(async function() {
+    const credentials = await generateTestData();
+    validVc = credentials.get('validVc');
+  });
+  // this will tell the report
+  // to make an interop matrix with this suite
+  this.matrix = true;
+  this.report = true;
+  this.implemented = [...verifierMatches.keys()];
+  this.rowLabel = 'Issuer';
+  this.columnLabel = 'Verifier';
+  for(const [issuerName, {endpoints}] of issuerMatches) {
+    let issuedVc;
+    before(async function() {
+      const [issuer] = endpoints;
+      issuedVc = await createInitialVc({issuer, vc: validVc});
+    });
+    for(const [verifierName, {endpoints}] of verifierMatches) {
+      const [verifier] = endpoints;
+      it(`${verifierName} should verify ${issuerName}`, async function() {
+        this.test.cell = {rowId: issuerName, columnId: verifierName};
+        const body = {
+          verifiableCredential: issuedVc,
+          options: {
+            checks: ['proof']
+          }
+        };
+        const {result, error} = await verifier.post({json: body});
+        should.not.exist(error, 'Expected verifier to not error.');
+        should.exist(result, 'Expected result from verifier.');
+        should.exist(result.status, 'Expected verifier to return an HTTP' +
+          'status code');
+        result.status.should.equal(200, 'Expected HTTP status code to be 200.');
+      });
+    }
+  }
+});
