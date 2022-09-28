e4045787a4157f6905cf5e95cc1896520d91fb57
Andrew Jones <ajones@digitalbazaar.com>
2022-09-28 14:02:33 -0400

    1 failure need to finish the invalid hash test.

---
+++ tests/vc-generator/helpers.js
@@ -0,0 +1,14 @@
+import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
+import {decodeSecretKeySeed} from 'bnid';
+
+export const getMultikey = async ({seedMultibase}) => {
+  if(!seedMultibase) {
+    throw new Error('seedMultibase required');
+  }
+  // convert multibase seed to Uint8Array
+  const seed = decodeSecretKeySeed({secretKeySeed: seedMultibase});
+  const key = await Ed25519Multikey.generate({seed});
+  const signer = key.signer();
+  signer.id = `did:key:${key.publicKeyMultibase}`;
+  return signer;
+};
