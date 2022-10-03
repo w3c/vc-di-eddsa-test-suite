2d8cee6696992f9b687a24280954286b629c7d0e
Andrew Jones <ajones@digitalbazaar.com>
2022-10-03 11:18:00 -0400

    Use _issuedClone to DRY up issuing logic.

--- tests/vc-generator/generators.js
+++ tests/vc-generator/generators.js
@@ -23,82 +23,69 @@ export const vcGenerators = new Map([
   ['noCreated', _noCreated],
   ['invalidCreated', _invalidCreated],
   ['noVm', _noVm],
-  ['noProofPurpose', _noProofPurpose]
+  ['invalidVm', _invalidVm],
+  ['noProofPurpose', _noProofPurpose],
+  ['invalidProofPurpose', _invalidProofPurpose]
 ]);
+
+async function _invalidProofPurpose({signer, credential}) {
+  const suite = _createEddsa2022Suite({signer});
+  const signedVc = await _issueCloned({suite, credential});
+  return signedVc;
+}
 
 async function _noProofPurpose({signer, credential}) {
   const suite = _createEddsa2022Suite({signer});
   suite.createProof = invalidCreateProof({addProofPurpose: false});
-  const signedVc = await vc.issue({
-    credential: klona(credential),
-    suite,
-    documentLoader
-  });
+  const signedVc = await _issueCloned({suite, credential});
+  return signedVc;
+}
+
+async function _invalidVm({signer, credential}) {
+  const suite = _createEddsa2022Suite({signer});
+  suite.verificationMethod = 'did:key:@invalidVm@';
+  const signedVc = await _issueCloned({suite, credential});
   return signedVc;
 }
 
 async function _noVm({signer, credential}) {
   const suite = _createEddsa2022Suite({signer});
   suite.createProof = invalidCreateProof({addVm: false});
-  const signedVc = await vc.issue({
-    credential: klona(credential),
-    suite,
-    documentLoader
-  });
+  const signedVc = await _issueCloned({suite, credential});
   return signedVc;
 }
 
 async function _invalidCreated({signer, credential}) {
   const suite = _createEddsa2022Suite({signer});
   suite.date = 'invalidDate';
-  const signedVc = await vc.issue({
-    credential: klona(credential),
-    suite,
-    documentLoader
-  });
+  const signedVc = await _issueCloned({suite, credential});
   return signedVc;
 }
 
 async function _noCreated({signer, credential}) {
   const suite = _createEddsa2022Suite({signer});
   suite.createProof = invalidCreateProof({addCreated: false});
-  const signedVc = await vc.issue({
-    credential: klona(credential),
-    suite,
-    documentLoader
-  });
+  const signedVc = await _issueCloned({suite, credential});
   return signedVc;
 }
 
 async function _incorrectCryptosuite({signer, credential}) {
   const suite = _createEddsa2022Suite({signer});
   suite.cryptosuite = 'unknown-cryptosuite-2017';
-  const signedVc = await vc.issue({
-    credential: klona(credential),
-    suite,
-    documentLoader
-  });
+  const signedVc = await _issueCloned({suite, credential});
   return signedVc;
 }
 
 async function _incorrectProofType({signer, credential}) {
   const suite = _createEddsa2022Suite({signer});
   suite.type = 'UnknownProofType';
-  const signedVc = await vc.issue({
-    credential: klona(credential),
-    suite,
-    documentLoader
-  });
+  const signedVc = await _issueCloned({suite, credential});
   return signedVc;
 }
 
 async function _issuedVc({signer, credential}) {
   const suite = _createEddsa2022Suite({signer});
-  const signedVc = await vc.issue({
-    credential: klona(credential),
-    suite,
-    documentLoader
-  });
+  const signedVc = await _issueCloned({suite, credential});
   return signedVc;
 }
 
@@ -109,22 +96,14 @@ async function _incorrectCanonize({signer, credential}) {
     // this will canonize using JCS
     return canonicalize(input);
   };
-  const signedVc = await vc.issue({
-    credential: klona(credential),
-    suite,
-    documentLoader
-  });
+  const signedVc = await _issueCloned({suite, credential});
   return signedVc;
 }
 
 async function _incorrectDigest({signer, credential}) {
   const suite = _createEddsa2022Suite({signer});
   suite.createVerifyData = invalidCreateVerifyData;
-  const signedVc = await vc.issue({
-    credential: klona(credential),
-    suite,
-    documentLoader
-  });
+  const signedVc = await _issueCloned({suite, credential});
   return signedVc;
 }
 
@@ -134,3 +113,12 @@ function _createEddsa2022Suite({signer}) {
   const cryptosuite = eddsa2022CryptoSuite;
   return new DataIntegrityProof({signer, date, cryptosuite});
 }
+
+async function _issueCloned({suite, credential, loader = documentLoader}) {
+  return vc.issue({
+    credential: klona(credential),
+    suite,
+    documentLoader: loader
+  });
+}
+
