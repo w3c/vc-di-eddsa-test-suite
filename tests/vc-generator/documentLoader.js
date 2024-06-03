90e84ec2c55c60aa73c1477f8eb2605d64588e55
Andrew Jones <ajones@digitalbazaar.com>
2024-06-03 16:30:58 +0000

    Disable JsonLdDocumentLoader to prevent test data being marked static.

--- tests/vc-generator/documentLoader.js
+++ tests/vc-generator/documentLoader.js
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 import {contextMap} from './contexts.js';
+// disable JsonLdDocumentLoader to prevent test data being marked static.
+/*
 import {JsonLdDocumentLoader} from 'jsonld-document-loader';
 const jdl = new JsonLdDocumentLoader();
 
@@ -11,5 +13,16 @@ for(const [key, value] of contextMap) {
   jdl.addStatic(key, value);
 }
 
-const documentLoader = jdl.build();
-export {documentLoader};
+export const documentLoader = jdl.build();
+*/
+export const documentLoader = url => {
+  const document = contextMap.get(url);
+  if(!document) {
+    throw new Error(`Document not found in document loader: ${url}`);
+  }
+  return {
+    contextUrl: null,
+    documentUrl: url,
+    document
+  };
+};
