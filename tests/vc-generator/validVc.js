82da9101bc80d2dff054886a3bf2ac52c06f6509
Andrew Jones <ajones@digitalbazaar.com>
2024-11-01 23:07:56 +0000

    Switch to VC 2.0 as default validVc.

--- tests/vc-generator/validVc.js
+++ tests/vc-generator/validVc.js
@@ -3,12 +3,11 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 export const validVc = {
-  '@context': ['https://www.w3.org/2018/credentials/v1'],
+  '@context': ['https://www.w3.org/ns/credentials/v2'],
   id: 'urn:uuid:86294362-4254-4f36-854f-3952fe42555d',
   type: ['VerifiableCredential'],
-  issuer: 'did:key:z6MksNZwi2r6Qxjt3MYLrrZ44gs2fauzgv1dk4E372bNVjtc',
-  issuanceDate: '2020-03-16T22:37:26.544Z',
   credentialSubject: {
-    id: 'did:key:z6MktKwz7Ge1Yxzr4JHavN33wiwa8y81QdcMRLXQsrH9T53b'
+    id: 'did:key:z6MktKwz7Ge1Yxzr4JHavN33wiwa8y81QdcMRLXQsrH9T53b',
+    name: 'ExampleClaim'
   }
 };
