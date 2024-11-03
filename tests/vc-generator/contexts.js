04dd14106da1ac5b35e44e86a1bfe81df744b31b
Andrew Jones <ajones@digitalbazaar.com>
2024-11-03 16:44:28 -0500

    Add invalidPurpose to VC 2.0 context.

--- tests/vc-generator/contexts.js
+++ tests/vc-generator/contexts.js
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 import {
-  contexts as credentialsContexts
+  contexts as credentialsContexts,
+  named as namedCredentialsContexts
 } from '@digitalbazaar/credentials-context';
 import dataIntegrityCtx from '@digitalbazaar/data-integrity-context';
 import didCtx from '@digitalcredentials/did-context';
@@ -12,12 +13,18 @@ import multikeyCtx from '@digitalbazaar/multikey-context';
 const contextMap = new Map(credentialsContexts);
 
 const _dataIntegrityCtx = structuredClone(dataIntegrityCtx.CONTEXT);
+const {
+  id: v2ContextUrl,
+  context: v2Context
+} = structuredClone(namedCredentialsContexts.get('v2'));
+const v2Ctx = v2Context['@context'];
 const diCtx = _dataIntegrityCtx['@context'];
 // add UnknownProofType to local context for test data
 diCtx.UnknownProofType =
   structuredClone(_dataIntegrityCtx['@context'].DataIntegrityProof);
 // add invalidPurpose to context for test data
-diCtx.DataIntegrityProof['@context'].proofPurpose['@context'].invalidPurpose = {
+diCtx.DataIntegrityProof['@context'].proofPurpose['@context'].invalidPurpose =
+v2Ctx.DataIntegrityProof['@context'].proofPurpose['@context'].invalidPurpose = {
   '@id': 'https://w3id.org/security#invalidPurpose',
   '@type': '@id',
   '@container': '@set'
@@ -33,6 +40,10 @@ contextMap.set(
   didCtx.constants.DID_CONTEXT_URL,
   didCtx.contexts.get(
     didCtx.constants.DID_CONTEXT_URL)
+);
+contextMap.set(
+  v2ContextUrl,
+  v2Context
 );
 
 export {contextMap};
