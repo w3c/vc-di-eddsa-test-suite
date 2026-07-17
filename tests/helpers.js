975a0f1015be3d6d0bfceb465429d1355a453dc4
BigBlueHat <byoung@bigbluehat.com>
2026-07-17 17:25:19 -0400

    Remove uuid in favor of node:crypto.

--- tests/helpers.js
+++ tests/helpers.js
@@ -7,8 +7,8 @@ import * as Ed25519Multikey from '@digitalbazaar/ed25519-multikey';
 import {IdDecoder, IdEncoder} from 'bnid';
 import {documentLoader} from './documentLoader.js';
 import {isUtf8} from 'node:buffer';
+import {randomUUID} from 'node:crypto';
 import {readFileSync} from 'fs';
-import {v4 as uuidv4} from 'uuid';
 import varint from 'varint';
 
 export const config = JSON.parse(readFileSync('./config/runner.json'));
@@ -69,7 +69,7 @@ export async function getVerificationMethodDocuments(proofs) {
 export const createInitialVc = async ({issuer, vc}) => {
   const {settings: {id: issuerId, options}} = issuer;
   const credential = structuredClone(vc);
-  credential.id = `urn:uuid:${uuidv4()}`;
+  credential.id = `urn:uuid:${randomUUID()}`;
   credential.issuer = issuerId;
   const body = {credential, options};
   const {data, error} = await issuer.post({json: body});
